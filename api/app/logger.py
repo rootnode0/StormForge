ae3857520b34f2887e385676009cfb0225517c87
albin732 <albingeov@gmail.com>
2026-04-26 14:09:14 +0530

    timestamp in log

--- api/app/logger.py
+++ api/app/logger.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 LOG_DIR = "/app/logs"
 
@@ -6,5 +7,7 @@ def log_message(mission_id, message):
     os.makedirs(LOG_DIR, exist_ok=True)
     file_path = f"{LOG_DIR}/{mission_id}.log"
 
+    timestamp = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
     with open(file_path, "a") as f:
-        f.write(message + "\n")
+        f.write(f"[{timestamp}] {message}\n")
